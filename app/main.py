22879b525c9f0f2db5fd9ac9b2b9e948727fdad3
xvanausloos <xavier.vanausloos@ladonneeintelligente.com>
2024-10-29 07:52:45 +0100

    feature: added Parquet file

--- app/main.py
+++ app/main.py
@@ -3,6 +3,9 @@ from pyspark.sql.functions import *
 
 if __name__ == '__main__':
     spark = SparkSession.builder.appName("spark-performance").master("local[*]").getOrCreate()
-    #df = spark.read.parquet("/data/drv/stocks/AAPL")
-    df = spark.read.csv("../data/drv/stocks/aapl.csv", sep=",", header=True)
+    df = spark.read.parquet("../data/drv/stocks/appl")
+    #df = spark.read.csv("../data/drv/stocks/aapl.csv", sep=",", header=True, inferSchema=True)
     df.show()
+
+    # convert CSV in PARQUET
+    # df.write.parquet("../data/drv/stocks/appl")
